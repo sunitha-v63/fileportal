65fa798480aca0959e4d9c89d97f40c7cd822e8b
sunitha-d63 <sunithagopal06@gmail.com>
2025-12-07 23:18:18 +0530

    a

--- api/index.py
+++ api/index.py
@@ -1,9 +1,16 @@
 import os
+import sys
 from django.core.asgi import get_asgi_application
+
+# Add project root to PYTHONPATH (important for Vercel)
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "secureportal.settings")
 
-app = get_asgi_application()
+django_app = get_asgi_application()
 
 async def handler(scope, receive, send):
-    await app(scope, receive, send)
+    """
+    Vercel serverless ASGI entrypoint.
+    """
+    await django_app(scope, receive, send)
