91979c2c025a84df8cefc173aeb5bc545e282ffd
sunitha-d63 <sunithagopal06@gmail.com>
2025-12-07 23:15:35 +0530

    as

--- secureportal/settings.py
+++ secureportal/settings.py
@@ -108,7 +108,7 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
-DEFAULT_FROM_EMAIL = 'sumisunitha06@gmail.com'
+DEFAULT_FROM_EMAIL = os.environ.get("EMAIL_HOST_USER")
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
